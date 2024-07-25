27ab30efbe85ff5b8d0abe95bae40a269515a8a5
hky1999 <976929993@qq.com>
2024-07-25 20:35:23 +0800

    wip: introduce axvmm but failed

--- arceos-vmm/src/main.rs
+++ arceos-vmm/src/main.rs
@@ -34,14 +34,20 @@ pub static mut AXVM_PER_CPU: AxVMPerCpu<AxVMHalImpl> = AxVMPerCpu::new_uninit();
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     println!("Starting virtualization...");
+
+    // TODO: remove this to somewhere else like `percpu.hardware_enable()`.
     info!("Hardware support: {:?}", axvm::has_hardware_support());
 
+    // Init hardware virtualization support in each core.
+    // Note: This is awkward because we need to find a proper place to call this on each core.
     let percpu = unsafe { AXVM_PER_CPU.current_ref_mut_raw() };
     percpu.init(0).expect("Failed to initialize percpu state");
     percpu
         .hardware_enable()
         .expect("Failed to enable virtualization");
 
+    // Config file for guest VM should be read into memory in a more flexible way.
+    // FIXME: remove this hardcode.
     let raw_vm_config = core::include_str!("../configs/nimbos.toml");
     let vm_create_config =
         AxVMCrateConfig::from_toml(raw_vm_config).expect("Failed to resolve VM config");
@@ -54,6 +60,9 @@ fn main() {
     let vm = AxVM::<AxVMHalImpl>::new(config, 0, gpm.nest_page_table_root())
         .expect("Failed to create VM");
     info!("Boot VM...");
+
+    // Todo: remove this, details can be get from 
+    // this [PR](https://github.com/arceos-hypervisor/arceos-umhv/pull/5). 
     vm.boot().unwrap();
     panic!("VM boot failed")
 }
