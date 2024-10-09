5d21462091d129bab28651f7090af29fe193cbf0
Debin <luodeb@outlook.com>
2024-10-09 15:27:36 +0800

    download linux-aarch64.bin from github

--- arceos-vmm/scripts/make/rk3588.mk
+++ arceos-vmm/scripts/make/rk3588.mk
@@ -1,6 +1,17 @@
+IMAGE_FILE = ../linux-aarch64.bin
+GITHUB_URL = https://github.com/luodeb/rk3588-linux-bin/raw/main/linux-aarch64.bin
+
+download:
+	@if [ ! -e $(IMAGE_FILE) ]; then \
+		echo "linux-aarch64.bin not found, downloading from $(GITHUB_URL)..."; \
+		curl -L -o $(IMAGE_FILE) $(GITHUB_URL); \
+	else \
+		echo "linux-aarch64.bin already exists."; \
+	fi
+
 rk3588.dtb:
 	dtc -I dts -O dtb -o ../rk3588.dtb ../rk3588.dts
 
-kernel: rk3588.dtb build 
+kernel: download rk3588.dtb build 
 	./tools/rk3588/mkimg --dtb rk3588-firefly-itx-3588j.dtb --img $(OUT_BIN)
 	@echo 'Built the FIT-uImage boot.img'
