192db3ff3bf954afe36b2040f4dcb4bc1c03bb36
hky1999 <976929993@qq.com>
2025-01-02 17:03:32 +0800

    [refactor] generate linker dynamically

--- arceos-vmm/build.rs
+++ arceos-vmm/build.rs
@@ -204,10 +204,14 @@ fn generate_guest_img_loading_functions(
 }
 
 fn main() -> io::Result<()> {
+    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
     let platform = env::var("AX_PLATFORM").unwrap_or("".to_string());
-    let platform_family = env::var("AX_PLATFORM").unwrap_or("".to_string());
     println!("cargo:rustc-cfg=platform=\"{}\"", platform);
-    println!("cargo:rustc-cfg=platform_family=\"{}\"", platform_family);
+
+    if platform != "dummy" {
+        gen_linker_script(&arch, platform.as_str()).unwrap();
+    }
 
     let config_files = get_configs();
     let mut output_file = open_output_file();
@@ -245,5 +249,34 @@ fn main() -> io::Result<()> {
             writeln!(output_file, "}}\n")?;
         }
     }
+    Ok(())
+}
+
+fn gen_linker_script(arch: &str, platform: &str) -> io::Result<()> {
+    use axstd::os::arceos::modules::axconfig;
+
+    let fname = format!("linker_{}.lds", platform);
+    let output_arch = if arch == "x86_64" {
+        "i386:x86-64"
+    } else if arch.contains("riscv") {
+        "riscv" // OUTPUT_ARCH of both riscv32/riscv64 is "riscv"
+    } else {
+        arch
+    };
+    let ld_content = std::fs::read_to_string("scripts/lds/linker.lds.S")?;
+    let ld_content = ld_content.replace("%ARCH%", output_arch);
+    let ld_content = ld_content.replace(
+        "%KERNEL_BASE%",
+        &format!("{:#x}", axconfig::plat::KERNEL_BASE_VADDR),
+    );
+    let ld_content = ld_content.replace("%SMP%", &format!("{}", axconfig::SMP));
+
+    // target/<target_triple>/<mode>/build/arceos-vmm-xxxx/out
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    // target/<target_triple>/<mode>/linker_xxxx.lds
+    let out_path = Path::new(&out_dir).join("../../../").join(fname);
+
+    println!("writing linker script to {}", out_path.display());
+    std::fs::write(out_path, ld_content)?;
     Ok(())
 }
